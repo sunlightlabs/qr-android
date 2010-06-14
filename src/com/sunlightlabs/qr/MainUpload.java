5f6412fe56fdebdc9ff989905f7266fb95eb199e
klondike <kprojection@gmail.com>
2010-06-14 16:01:15 -0400

    Got photo taking working

--- src/com/sunlightlabs/qr/MainUpload.java
+++ src/com/sunlightlabs/qr/MainUpload.java
@@ -1,5 +1,8 @@
 package com.sunlightlabs.qr;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+
 import android.app.Activity;
 import android.content.Intent;
 import android.database.Cursor;
@@ -7,18 +10,21 @@ import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.net.Uri;
 import android.os.Bundle;
+import android.os.Environment;
 import android.provider.MediaStore;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageView;
+import android.widget.Toast;
 
 public class MainUpload extends Activity {
     private static final int PICK_PHOTO = 1;
+    private static final int TAKE_PHOTO = 2;
     
 	private EditText message;
 	private ImageView photo;
-	private Button pickPhoto, print;
+	private Button pickPhoto, takePhoto, print;
 	
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -31,6 +37,7 @@ public class MainUpload extends Activity {
     public void setupControls() {
     	message = (EditText) findViewById(R.id.message);
     	pickPhoto = (Button) findViewById(R.id.pick_photo);
+    	takePhoto = (Button) findViewById(R.id.take_photo);
     	photo = (ImageView) findViewById(R.id.photo);
     	print = (Button) findViewById(R.id.print);
     	
@@ -42,6 +49,15 @@ public class MainUpload extends Activity {
 				startActivityForResult(intent, PICK_PHOTO);
 			}
 		});
+    	
+    	takePhoto.setOnClickListener(new View.OnClickListener() {
+			@Override
+			public void onClick(View v) {
+				Intent intent = new Intent("android.media.action.IMAGE_CAPTURE")
+					.putExtra(android.provider.MediaStore.EXTRA_OUTPUT, Uri.fromFile(new File(tempFile())));
+				startActivityForResult(intent, TAKE_PHOTO);
+			}
+		});
     }
     
     @Override
@@ -50,21 +66,47 @@ public class MainUpload extends Activity {
 		case PICK_PHOTO:
 			if (resultCode == RESULT_OK) {
 				Uri selectedImage = data.getData();
-	            String[] filePathColumn = {MediaStore.Images.Media.DATA};
-
-	            Cursor cursor = getContentResolver().query(selectedImage, filePathColumn, null, null, null);
-	            cursor.moveToFirst();
-
-	            int columnIndex = cursor.getColumnIndex(filePathColumn[0]);
-	            String filePath = cursor.getString(columnIndex);
-	            cursor.close();
-	            
-	            Bitmap chosenPhoto = BitmapFactory.decodeFile(filePath);
-	            photo.setVisibility(View.VISIBLE);
-	            photo.setImageBitmap(chosenPhoto);
+	            photo.setImageBitmap(bitmapFromUri(selectedImage));
+			}
+			break;
+		case TAKE_PHOTO:
+			if (resultCode == RESULT_OK) {
+				Uri takenImage;
+				File file = new File(tempFile());
+                try {
+                	String result = android.provider.MediaStore.Images.Media.insertImage(getContentResolver(), file.getAbsolutePath(), null, null);
+                    takenImage = Uri.parse(result);
+                    
+                	if (!file.delete())
+                        Toast.makeText(this, "Failed to delete " + file, Toast.LENGTH_SHORT);
+                    
+                } catch (FileNotFoundException e) {
+                	takenImage = null;
+                    Toast.makeText(this, "Photo file not found!", Toast.LENGTH_SHORT);
+                }
+                
+                if (takenImage != null)
+                	photo.setImageBitmap(bitmapFromUri(takenImage));
 			}
 			break;
 		}
+    }
+    
+    private String tempFile() {
+    	return Environment.getExternalStorageDirectory().getPath() + "/tempImage";
+    }
+    
+    private Bitmap bitmapFromUri(Uri uri) {
+    	String[] filePathColumn = {MediaStore.Images.Media.DATA};
+
+        Cursor cursor = getContentResolver().query(uri, filePathColumn, null, null, null);
+        cursor.moveToFirst();
+
+        int columnIndex = cursor.getColumnIndex(filePathColumn[0]);
+        String filePath = cursor.getString(columnIndex);
+        cursor.close();
+        
+        return BitmapFactory.decodeFile(filePath);
     }
     
 }

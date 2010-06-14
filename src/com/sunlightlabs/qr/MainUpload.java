825262d7dc6801ee28a9c9d7a2a5142604799bf5
klondike <kprojection@gmail.com>
2010-06-14 12:19:15 -0400

    Initial commit

---
+++ src/com/sunlightlabs/qr/MainUpload.java
@@ -0,0 +1,13 @@
+package com.sunlightlabs.qr;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class MainUpload extends Activity {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
